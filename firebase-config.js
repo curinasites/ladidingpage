75acfe5ac3320da5d4e27cbb6e06ebe43c4a1f3f
Curinasites <oquenaofazermodsff@gmail.com>
2026-05-17 01:11:34 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,17 @@
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDD_sA1Fu-_WWipHT8X3cut-TwFp6zVmyk",
-    authDomain: "loja-apks.firebaseapp.com",
-    databaseURL: "https://loja-apks-default-rtdb.firebaseio.com",
-    projectId: "loja-apks",
-    storageBucket: "loja-apks.firebasestorage.app",
-    messagingSenderId: "853364224426",
-    appId: "1:853364224426:web:ea57f39afc9103f25bfbd3"
+// ========== CONFIGURAÇÃO FIREBASE - LANDING PAGE ==========
+const firebaseConfig = {
+  apiKey: "AIzaSyAoTpeweJAuluIYFwLkVhiUVso8yxU8T_s",
+  authDomain: "francisco-sites.firebaseapp.com",
+  databaseURL: "https://francisco-sites-default-rtdb.firebaseio.com",
+  projectId: "francisco-sites",
+  storageBucket: "francisco-sites.firebasestorage.app",
+  messagingSenderId: "386716924748",
+  appId: "1:386716924748:web:7656bac0936c68ba416de9",
+  measurementId: "G-GGE9RE58G2"
 };
 
-firebase.initializeApp(FIREBASE_CONFIG);
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
+const db = firebase.firestore();
+const auth = firebase.auth();
